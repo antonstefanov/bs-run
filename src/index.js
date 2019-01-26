fe37a0c9e9a4c9e5bc85155d2c599b2273509e8e
Anton <anton.stefanov@live.com>
2019-01-26 21:40:05 +1100

    code runner selection errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@
 
 const args = require("./util/args");
 const run = require("./node/run");
+const path = require("path");
 
 (async () => {
   const executedFile = args.cmd(process.argv);
+  if (executedFile == null) {
+    console.error("Must specify which file to execute");
+    return;
+  } else if (
+    path.basename(executedFile, path.extname(executedFile)) ===
+    "tempCodeRunnerFile"
+  ) {
+    console.error("Partial code-runner selections are not supported");
+    return;
+  }
   await run.run(executedFile);
   // do more things
 })();
--- src/node/run.js
+++ src/node/run.js
@@ -2,9 +2,6 @@ const bs = require("./bs");
 const path = require("path");
 
 const getInfo = async executedFile => {
-  if (executedFile == null) {
-    throw new Error("Must specify which file to execute");
-  }
   const absoluteExecutedFile = path.resolve(__dirname, executedFile);
   const fileInfo = path.parse(absoluteExecutedFile);
   const bsConfigPath = await bs.closestBsConfigPathAsync(fileInfo.dir);
